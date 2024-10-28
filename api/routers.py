72c2b348901f5b1bf9f53d9a5c10933d8fc6a9bc
Sergey <serg.mayorov316@gmail.com>
2024-10-28 19:52:40 +1000

    Добавлена сырая реализация запроса на выборку продаж по условиям

--- api/routers.py
+++ api/routers.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException
 from database import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, insert, update, delete, Table
-from models.models import cities, trades, products, stores
+from sqlalchemy import select, insert, update, delete, Table, join, and_, func
+from models.models import cities, trades, products, stores, trades_products
 from typing import List, Dict
-from schemas.schemas import City, Trade, Store, Product
+from schemas.schemas import City, Trade, Store, Product, TradeFilter
 from pydantic import BaseModel
+from datetime import datetime, timedelta
 
 
 class CRUDableEntity:
@@ -180,3 +181,38 @@ async def update_trade(new_trade: Trade, entity: CRUDableEntity = Depends(lambda
 async def delete_trade(trade_id: int, entity: CRUDableEntity = Depends(lambda: trade), session: AsyncSession = Depends(get_async_session)):
     result = await entity.delete(trade_id, session)
     return result
+
+
+# To-do: дотестировать
+@router.get("/get_filtered_trades")
+async def get_filtered_trades(filters: TradeFilter = Depends(), session: AsyncSession = Depends(get_async_session)):
+    conditions = []
+    if filters.city_id is not None:
+        conditions.append(stores.c.city_id == filters.city_id)
+    if filters.product_id is not None:
+        conditions.append(trades.c.product_id == filters.product_id)
+    if filters.store_id is not None:
+        conditions.append(trades.c.store_id == filters.store_id)
+    if filters.last_n_days is not None:
+        last_date = datetime.now() - timedelta(days=filters.last_n_days)
+        conditions.append(trades.c.date >= last_date)
+    query = select(trades).select_from(join(trades, trades_products, trades.c.id == trades_products.c.trade_id)
+                                       .join(products, products.c.id == trades_products.c.product_id)
+                                       .join(stores, trades.c.store_id == stores.c.id)).where(
+                                           and_(
+                                               *conditions
+                                           )
+                                       ).group_by(
+                                           trades.c.id
+                                       )
+    if filters.min_total is not None:
+        query = query.having(func.sum(products.c.price) > filters.min_total)
+    if filters.max_total is not None:
+        query = query.having(func.sum(products.c.price) < filters.max_total)
+    if filters.min_count is not None:
+        query = query.having(func.count(products.c.id) > filters.min_count)
+    if filters.max_count is not None:
+        query = query.having(func.count(products.c.id) < filters.max_count)
+    
+    result = await session.execute(query)
+    return list(result.mappings())
--- schemas/schemas.py
+++ schemas/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
 
 
 class City(BaseModel):
@@ -23,3 +24,14 @@ class Product(BaseModel):
     id: int
     name: str
     price: int
+
+
+class TradeFilter(BaseModel):
+    city_id: Optional[int] = None
+    product_id: Optional[int] = None
+    store_id: Optional[int] = None
+    last_n_days: Optional[int] = None
+    min_total: Optional[int] = None
+    max_total: Optional[int] = None
+    min_count: Optional[int] = None
+    max_count: Optional[int] = None
